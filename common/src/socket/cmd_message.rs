dd22b6345955069646c9e801aec06c5f817897e9
mod skyline <1106554589@qq.com>
2023-12-06 20:41:53 +0800

    Changes

--- common/src/socket/cmd_message.rs
+++ common/src/socket/cmd_message.rs
@@ -1,1 +1,13 @@
+use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct CmdMessage {
+    pub cmd: Option<String>,
+    pub message: Option<String>,
+}
+
+impl CmdMessage {
+    pub fn new(cmd: Option<String>, message: Option<String>) -> Self {
+        Self { cmd, message }
+    }
+}
--- common/src/socket/cmd_message_grp_ids.rs
+++ common/src/socket/cmd_message_grp_ids.rs
@@ -1,1 +1,34 @@
+use serde::{Deserialize, Serialize};
 
+use crate::socket::cmd_message::CmdMessage;
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct CmdMessageGrpIds {
+    pub cmd: Option<String>,
+    pub message: Option<String>,
+    #[serde(rename = "grpIds")]
+    pub grp_ids: Option<Vec<i32>>,
+}
+
+impl CmdMessageGrpIds {
+    pub fn new(cmd: Option<String>, message: Option<String>, grp_ids: Option<Vec<i32>>) -> Self {
+        Self {
+            cmd,
+            message,
+            grp_ids,
+        }
+    }
+
+    pub fn new_with_cmd_message(cmd: CmdMessage, grp_ids: Option<Vec<i32>>) -> Self {
+        Self {
+            cmd: cmd.cmd,
+            message: cmd.message,
+            grp_ids,
+        }
+    }
+
+    //TODO:shuold return a Result or Option
+    pub fn get_cmd_message(&self) -> CmdMessage {
+        CmdMessage::new(self.cmd.clone(), self.message.clone())
+    }
+}
